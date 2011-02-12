0d7eeaaac4d1a25a15bdf80a93bb392eb22f5949
S Pradeep Kumar <gohanpra@gmail.com>
2011-02-13 01:00:25 +0530

    Evaluation seems to work normally.

--- Assi-4/infix-postfix.cpp
+++ Assi-4/infix-postfix.cpp
@@ -9,9 +9,16 @@
   7) 5+((1+2)*4)-3 gave output 5 1 2 + 4 * + 3 - instead of 5 1 2 + 4 * 3 - + but i think both r same :-)
   8) 3^4+5*6^2-1
 
+  Pradeep:
+  1. ( )
+
 */
 
-#include<iostream>
+
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
 #include"stack.cpp"
 using namespace std;
 
@@ -19,48 +26,93 @@ class expression
 {
      char infix_arr[100], postfix_arr[100];
      Stack oper_stack;
+     Stack value_stack;
      int value;
 
 public:     
      void get_infix_expression (); 		// Which will get user postfix_arr into infix_str //to be done
      void convert_to_postfix ();   		// Which will convert infix_str to postfix and put result in postfix_str
-     void evaluate_postfix_expression ();	//to be done
-//     void evaluate (){
-//	  convert_to_postfix ();		//to be done
-//	  evaluate_postfix_expression ();
-
+     int evaluate_postfix_expression ();	//to be done
+     void evaluate (){
+	  convert_to_postfix ();		//to be done
+	  evaluate_postfix_expression ();
+     }
+     
 //     void write_to_file (string filename);	//to be done
-//     void print ();				// Just cout all the value
+     void print ();				// Just cout all the value
      void display();				//cout postfix string, for can be removed after whole prgm is written
 };
 
-     
+void expression::print ()
+{
+     cout << "Values : " << endl;
+     cout << infix_arr << endl;
+     cout << postfix_arr << endl;
+     cout << value << endl;
+}
+
+
 int precedence(char c)
 {
      int p;
      switch(c)
      {
-     case '(':p=1;
+     case '(':
+	  p=1;
 	  break;
-     case '-':p=2;
+     case '-':
+	  p=2;
 	  break;
-     case '+':p=2;
+     case '+':
+	  p=2;
 	  break;
 		
-     case '*':p=3;
+     case '*':
+	  p=3;
 	  break;
-     case '/':p=3;
+     case '/':
+	  p=3;
 	  break;
-     case '^':p=4;
+     case '^':
+	  p=4;
 	  break;
      }
 
      return p;
 }
 
+int eval_operation (int next_token, int value1, int value2)
+{
+     int result;
+     switch(next_token)
+     {
+     case '-':
+	  return (value1 - value2);
+	  break;
+     case '+':
+	  return (value1 + value2);
+	  break;
+     case '*':
+	  return (value1 * value2);
+	  break;
+     case '/':
+	  return (value1 / value2);
+	  break;
+     case '^':
+	  result = 1;
+	  for (int i = 0; i < value2; i++){
+	       result *= value1;
+	  }
+	  return result;
+	  break;
+     default:
+	  cerr << "Invalid operator!" << endl;
+     }
+}
+
 bool is_operator(char a)
 {
-     if(((a!='*')&&(a!='+')&&(a!='-')&&(a!='/')&&(a!='^')&&(a!='(')&&(a!=')')))
+     if((a!='*')&&(a!='+')&&(a!='-')&&(a!='/')&&(a!='^')&&(a!='(')&&(a!=')'))
 	  return false;
      else
 	  return true;
@@ -100,7 +152,7 @@ void expression::convert_to_postfix()
      char c,temp;
      int i = 0,j = 0,size=0,flag = 0;
 
-     cout<<infix_arr<<endl;
+     cout<<"Given infix expression : " << infix_arr<<endl;
 
      while(infix_arr[i]!='\0')
      {
@@ -176,22 +228,71 @@ void expression::convert_to_postfix()
 	  i++;
 		
      }
+	  
      while(!oper_stack.isempty())
      {
 	  c = char (oper_stack.pop());
 	  postfix_arr[j++] = c;
 	  postfix_arr[j++] = ' ';
      }
-     postfix_arr[--j] = '\0';
+     if (j > 0)
+	  postfix_arr[--j] = '\0';
+     else
+	  postfix_arr[0] = '\0';
 }
 
-void evaluate_postfix_expression ()
+int expression::evaluate_postfix_expression ()
 {
-     
-     // while (i < arr_size){
-     // 	  next_token = postfix_arr[i];
-     // 	  if (is_numeral (next_token)){
-	       
+     int i, j, arr_size = strlen (postfix_arr);
+     cout << postfix_arr << endl;
+     int value1, value2, result;
+     int next_token;
+     if (arr_size == 0){
+	  cerr << "Nothing to evaluate" << endl;
+	  this->value = 0;
+	  return -1;
+     }
+
+     i = 0;
+     string s;
+     while (i < arr_size){
+	  cout << "i : " << i << " postfix_arr[i] : " << postfix_arr[i] << endl;
+	  if (postfix_arr[i] == ' ')
+	       ;
+     	  else if (!is_operator (postfix_arr[i])){
+	       s = "";
+	       j = i;
+	       while (j < arr_size && postfix_arr[j] != ' ')
+		    s.push_back (postfix_arr[j++]);
+	       i = j; 
+	       cout << "s : " << s << endl;
+	       next_token = atoi (s.c_str ());
+	       value_stack.push (next_token);
+	  }
+	  else{
+	       next_token = postfix_arr[i];
+	       cout << "next_token : " << next_token << endl;
+	       if (value_stack.get_size () < 2){
+		    cerr << "Error : Not enough operands!" << endl;
+		    value = -1;
+		    return -1;
+	       }
+	       value2 = value_stack.pop ();
+	       value1 = value_stack.pop ();
+	       result = eval_operation (next_token, value1, value2);
+	       value_stack.push (result);
+	  }
+	  i++;
+     }
+
+     if (value_stack.get_size () == 1){
+	  this->value = value_stack.pop ();
+     }
+     else{
+	  cerr << "Error : Not enough operators!" << endl;
+	  value = 0;
+	  return -1;
+     }
 }
 
 
@@ -199,9 +300,11 @@ main()
 {
      expression e;
      e.get_infix_expression ();
-     e.convert_to_postfix ();		//for testing can be removed later when whole prgm is finished
-     e.display();				//for testing
-//	e.evaluate ();
+     // e.convert_to_postfix ();		//for testing can be removed later when whole prgm is finished
+     // e.display();				//for testing
+     // e.print ();
+     e.evaluate ();
+     e.print ();
 //	e.write_to_file (inputFile);
 }
 
